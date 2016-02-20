40caa56f08ed147ba70ee9e854075effddbb21cf
Timidger <APragmaticPlace@gmail.com>
2016-02-19 19:54:49 -0500

    Added several more display options, including hiding input and not waiting for EOF to process input. Also the chosen function

--- src/display.c
+++ src/display.c
@@ -2,14 +2,22 @@
 #include "player.h"
 #include "items.h"
 
+#include <stdbool.h>
+#include <termios.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 
 
 #define COLOR_PRELUDE "<" ANSI_COLOR_GREEN
 #define END_COLOR ANSI_COLOR_RESET ">" 
 #define COLOR_ITEM COLOR_PRELUDE ANSI_COLOR_GREEN
+
+
+struct termios OLD, NEW;
+bool TERMINAL_INPUT_HIDDEN = false;
 
 void reset_format(void) {
     printf(ANSI_COLOR_RESET);
@@ -48,3 +56,69 @@ char* get_item_name(item* item) {
     return output_string;
 }
 
+void hide_terminal_input(void) {
+    if (TERMINAL_INPUT_HIDDEN) {
+        return;
+    }
+    if (tcgetattr (fileno (stdout), &OLD) != 0) {
+        perror("Could not hide character input!");
+        return;
+    }
+    NEW = OLD;
+    NEW.c_lflag &= ~ECHO;
+    if (tcsetattr (fileno (stdout), TCSAFLUSH, &NEW) != 0) {
+        perror("Could not hide character input!");
+        return;
+    }
+    TERMINAL_INPUT_HIDDEN = true;
+}
+
+void show_terminal_input(void) {
+    if (! TERMINAL_INPUT_HIDDEN) {
+        return;
+    }
+    (void) tcsetattr (fileno (stdout), TCSAFLUSH, &OLD);
+    TERMINAL_INPUT_HIDDEN = false;
+}
+
+char* get_input(int limit) {
+    char input[limit];
+    fgets(input, limit, stdin);
+    // Remove the new line
+    input[strlen(input) - 1] = 0;
+    return strdup(input);
+}
+
+char* choose(char* options[], int num_of_options) {
+    printf("Choose one of the following:\n");
+    int max_length = 0;
+    int length = 0;
+    for (int i = 0; i < num_of_options; i++) {
+        printf("\t%d: " ANSI_BOLD "%s" ANSI_COLOR_RESET "\n", i+1, options[i]);
+        // Plus one cause null ptr
+        length = strlen(options[i]) + 1;
+        if (length > max_length) {
+            // And another cause fgets assumes another
+            max_length = length + 1;
+        }
+    }
+    char input[max_length];
+    char* input_ptr = input;
+    while (true) {
+        input_ptr = get_input(max_length);
+        for (int i = 0; i < num_of_options; i++) {
+            if (strcasecmp(options[i], input_ptr) == 0) {
+                return options[i];
+            }
+        }
+    }
+    perror("Fell out of infinite loop");
+    return NULL;
+}
+ 
+void raw_mode(void) {
+    system("/bin/stty raw");
+}
+void cooked_mode(void) {
+    system("/bin/stty cooked");
+}
--- src/display.h
+++ src/display.h
@@ -11,10 +11,20 @@
 #define ANSI_COLOR_MAGENTA "\x1b[35m"
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_GRAY    "\x1b[37m"
+
+#define ANSI_BLINK         "\e[5m"
+#define ANSI_BOLD          "\e[1m"
+
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
 void reset_format(void);
+void hide_terminal_input(void);
+void show_terminal_input(void);
+char* get_input(int limit);
 void print_items(player* player);
 void print_item(item* item);
 char* info(player* player);
 char* get_item_name(item* item);
+char* choose(char* options[], int num_of_options);
+void raw_mode(void);
+void cooked_mode(void);
